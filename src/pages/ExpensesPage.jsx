3106a07ba7c6c6ae1a341eacd57ee243c79372e8
Denisse <96150584+Denisse030@users.noreply.github.com>
2025-03-31 21:00:17 -0400

    Fourth commit

--- src/pages/ExpensesPage.jsx
+++ src/pages/ExpensesPage.jsx
@@ -4,42 +4,47 @@ import Table from "../components/Table";
 import { toast } from "react-toastify";
 
 export async function expensesLoader() {
-  const expenses = fetchData("expenses");
+  const userName = JSON.parse(localStorage.getItem("activeUser"));
+  const expenses = fetchData("expenses", userName) ?? [];
   return { expenses };
 }
 
-export async function expensesAction({request}) {
-    const data = await request.formData();
-    const {_action, ...values} = Object.fromEntries(data);
+export async function expensesAction({ request }) {
+  const data = await request.formData();
+  const { _action, ...values } = Object.fromEntries(data);
 
-    if (_action === "deleteExpense") {
-        try {
-          deleteItem({
-            key: "expenses",
-            id: values.expenseId,
-          })
-          return toast.success("Expense deleted")
-        } catch (e) {
-          throw new Error("There was a problem creating your expense.")
-        }
-      }
+  const userName = JSON.parse(localStorage.getItem("activeUser"));
+
+  if (_action === "deleteExpense") {
+    try {
+      deleteItem({
+        key: `expenses-${userName}`,
+        id: values.expenseId,
+      });
+      return toast.success("Expense deleted");
+    } catch (e) {
+      throw new Error("There was a problem deleting your expense.");
+    }
+  }
 }
 
 const ExpensesPage = () => {
-    const { expenses } = useLoaderData();
+  const { expenses } = useLoaderData();
 
-    return ( 
+  return (
     <div className="grid-lg">
-    <h1>All Expenses</h1>
-    {expenses && expenses.length > 0 ? (
-            <div className="grid-md">
-                <h2>Recent Expenses <small>({expenses.length} total)</small></h2>
-                <Table expenses={expenses} />
-            </div>
-        ) : (
-            <p>No Expenses to show</p>
-        )}
-  </div>
+      <h1>All Expenses</h1>
+      {expenses && expenses.length > 0 ? (
+        <div className="grid-md">
+          <h2>
+            Recent Expenses <small>({expenses.length} total)</small>
+          </h2>
+          <Table expenses={expenses} />
+        </div>
+      ) : (
+        <p>No expenses to show</p>
+      )}
+    </div>
   );
 };
 
